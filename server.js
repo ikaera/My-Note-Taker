f0efd33d03bdc2c60bedc3d2a25087a06186802c
ikaera <ikaera@gmail.com>
2023-02-25 13:03:57 -0500

    add get methods.

--- server.js
+++ server.js
@@ -1,30 +1,40 @@
 const express = require('express');
 const fs = require('fs/promises');
 const path = require('path');
-const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
+// const { clog } = require('./middleware/clog');
+// const api = require('./routes/index.js');
 
-const PORT = precess.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+// app.use('/api', api);
 
 app.use(express.static('public'));
 
 // HTML routes:
 // GET /notes should return the notes.html file.
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html')),
+);
 // GET * should return the index.html file.
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html')),
+);
 
 // API routes:
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
+app.get('/api/notes', (req, res) => res.json('db/db.json'));
+
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+app.post('/api/notes', (req, res) => {});
 
 app.listen(PORT, () =>
   console.log(`App is listening at http://localhost:${PORT} 🚀`),
 );
 // Bonus:
 // DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.//
+app.delete('/api/notes/:id', (req, res) => {});
