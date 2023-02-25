984793075efc44eb6dffde6977e5ee2a683d9a5c
ikaera <ikaera@gmail.com>
2023-02-25 11:57:52 -0500

    fix error.

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ app.use(express.static('public'));
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 
-app.listen(PORT, () => console.log(`App is listening at http://localhost:${PORT} 🚀`)
+app.listen(PORT, () =>
+  console.log(`App is listening at http://localhost:${PORT} 🚀`),
+);
 // Bonus:
-// DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
+// DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.//
